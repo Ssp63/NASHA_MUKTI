f8bb8f3f9570a6cd424833a4c28f55f7e2292a95
Ssp63 <138196855+Ssp63@users.noreply.github.com>
2025-03-26 15:14:03 +0530

    Update eventSlice.js

--- NashaMukti/NashaMukti/client/src/store/slices/eventSlice.js
+++ NashaMukti/NashaMukti/client/src/store/slices/eventSlice.js
@@ -1,6 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_API_URL;
 
 export const getEvents = createAsyncThunk(
   'events/getEvents',
